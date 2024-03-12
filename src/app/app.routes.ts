58cc3327a8530daf891378b9f3086210e74b8ace
PrusWielki <prusakpat@gmail.com>
2024-03-13 00:20:38 +0100

    homepage and loginpage setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginPageComponent },
+  { path: 'home', component: HomePageComponent },
+];
