004489812b9d247a8d45ca51892ef19ba728e021
Patryk <pruswielki@gmail.com>
2024-03-15 09:42:45 +0100

    add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: RegisterPageComponent },
   { path: '', component: HomePageComponent },
 ];
