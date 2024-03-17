552696b5dd0a93b1d15a38d67f408b3fd9911c4e
PrusWielki <prusakpat@gmail.com>
2024-03-17 19:07:43 +0100

    Note card progress

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   standalone: true,
   imports: [],
   templateUrl: './note-card.component.html',
-  styleUrl: './note-card.component.scss'
+  styleUrl: './note-card.component.scss',
 })
 export class NoteCardComponent {
-
+  @Input()
+  text!: string;
 }
