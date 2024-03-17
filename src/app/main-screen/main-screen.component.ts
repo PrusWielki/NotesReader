9f85b0b15de64d638383b4bae7999989b69f50c4
PrusWielki <prusakpat@gmail.com>
2024-03-17 19:23:00 +0100

    main screen progress

--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -10,7 +10,13 @@ import { NoteCardComponent } from '../note-card/note-card.component';
 })
 export class MainScreenComponent {
   cards = [
+    {
+      text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt, beatae voluptatibus. Officia at voluptates, facere animi minima, voluptatum explicabo molestiae repellat expedita eius libero quaerat aut reprehenderit nisi autem assumenda!',
+    },
     { text: 'asdasdasdsadasdasd' },
+    {
+      text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt, beatae voluptatibus. Officia at voluptates, facere animi minima, voluptatum explicabo molestiae repellat expedita eius libero quaerat aut reprehenderit nisi autem assumenda!',
+    },
     { text: 'asdasdasdsadasdasd' },
     { text: 'asdasdasdsadasdasd' },
   ];
