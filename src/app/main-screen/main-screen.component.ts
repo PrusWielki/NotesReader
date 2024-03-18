13f5965b7d5f913bb103721782118a28529d4b30
PrusWielki <prusakpat@gmail.com>
2024-03-18 12:05:58 +0100

    move navbar out of layout

--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NoteCardComponent } from '../note-card/note-card.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-main-screen',
   standalone: true,
-  imports: [NoteCardComponent],
+  imports: [NoteCardComponent, NavbarComponent],
   templateUrl: './main-screen.component.html',
   styleUrl: './main-screen.component.scss',
 })
@@ -17,6 +18,12 @@ export class MainScreenComponent {
     {
       text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt, beatae voluptatibus. Officia at voluptates, facere animi minima, voluptatum explicabo molestiae repellat expedita eius libero quaerat aut reprehenderit nisi autem assumenda!',
     },
+    { text: 'asdasdasdsadasdasd' },
+    { text: 'asdasdasdsadasdasd' },
+    { text: 'asdasdasdsadasdasd' },
+    { text: 'asdasdasdsadasdasd' },
+    { text: 'asdasdasdsadasdasd' },
+    { text: 'asdasdasdsadasdasd' },
     { text: 'asdasdasdsadasdasd' },
     { text: 'asdasdasdsadasdasd' },
   ];
--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+  styleUrl: './register-page.component.css',
 })
-export class RegisterPageComponent {
-
-}
+export class RegisterPageComponent {}
