40200c8e76a340bd93225f073f5413f6cbcf0016
Patryk <pruswielki@gmail.com>
2024-03-20 11:06:42 +0100

    add app engine adapter as comment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+// import adapter from 'svelte-adapter-appengine'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
