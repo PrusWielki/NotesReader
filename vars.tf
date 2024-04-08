9044e8bfdb930720bb6c158542a0a4baaeca534f
PrusWielki <prusakpat@gmail.com>
2024-04-09 00:47:07 +0200

    progress

---
+++ vars.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type    = string
+  default = "notesreader6"
+}
+
+variable "project_name" {
+  type    = string
+  default = "notesreader6"
+}
+
+variable "display_name" {
+  type    = string
+  default = "notesreader6"
+}
+
+variable "region" {
+  type    = string
+  default = "europe-central2"
+}
