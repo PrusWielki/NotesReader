0f12a78940f57a0658bcbd5a4ea00c353b0a6b02
PrusWielki <prusakpat@gmail.com>
2024-04-27 12:52:07 +0200

    change function location

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,9 @@
 
 import { onRequest } from 'firebase-functions/v2/https';
 import * as logger from 'firebase-functions/logger';
+import { setGlobalOptions } from 'firebase-functions/v2';
+
+setGlobalOptions({ region: 'europe-west1' });
 // import vision from '@google-cloud/vision';
 
 // const client = new vision.ImageAnnotatorClient();
