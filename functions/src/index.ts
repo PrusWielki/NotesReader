61c331e4ade4242392be8d818a7b21aa1fbd580d
PrusWielki <prusakpat@gmail.com>
2024-04-27 12:33:09 +0200

    firebase function

---
+++ functions/src/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import { onRequest } from 'firebase-functions/v2/https';
+import * as logger from 'firebase-functions/logger';
+import vision from '@google-cloud/vision';
+
+const client = new vision.ImageAnnotatorClient();
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const extractText = onRequest((request, response) => {
+	logger.info('Hello logs!', { structuredData: true });
+	response.send('Hello from Firebase!');
+});
