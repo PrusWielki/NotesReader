c8ae8efc328134d70897ac92215fc6dc190304b2
PrusWielki <prusakpat@gmail.com>
2024-04-27 15:15:15 +0200

    add text summaries

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,29 +1,41 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import { onRequest } from 'firebase-functions/v2/https';
 import * as logger from 'firebase-functions/logger';
 import { setGlobalOptions } from 'firebase-functions/v2';
+
+import { VertexAI } from '@google-cloud/vertexai';
+
+const project = 'notesreader11';
+const location = 'europe-west1';
+const textModel = 'gemini-1.0-pro';
+
+const vertexAI = new VertexAI({ project: project, location: location });
+
+const generativeModel = vertexAI.getGenerativeModel({
+	model: textModel,
+
+	generationConfig: { maxOutputTokens: 256 }
+});
 
 setGlobalOptions({ region: 'europe-west1' });
 import vision from '@google-cloud/vision';
 
 const client = new vision.ImageAnnotatorClient();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
 export const extractText = onRequest({ cors: true }, async (request, response) => {
-	// const value = await client.annotateImage(request.body);
 	const [textDetections] = await client.textDetection(request.body);
 	const fullTextAnnotation = textDetections.fullTextAnnotation;
+
 	logger.info(fullTextAnnotation?.text);
+
+	const requestVertex = {
+		contents: [
+			{ role: 'user', parts: [{ text: 'Summarize this text: ' + fullTextAnnotation?.text }] }
+		]
+	};
+	const result = await generativeModel.generateContent(requestVertex);
+	const responseVertex = result.response;
+
+	logger.info(JSON.stringify(responseVertex));
 
 	response.send({ text: fullTextAnnotation?.text });
 });
