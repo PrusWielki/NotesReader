f130c6e263e68cafede3e7f215102793e1f1b7d7
PrusWielki <prusakpat@gmail.com>
2024-04-27 19:32:03 +0200

    cleanup

--- src/lib/hooks/database-hooks.ts
+++ src/lib/hooks/database-hooks.ts
@@ -14,7 +14,6 @@ const saveDataToDb = async (image: File, text: string, summary: string, fileExt:
 		text: text,
 		summary: summary
 	};
-	// Add a new document in collection "cities" with ID 'LA'
 	const collectionRef = collection(db, 'userData');
 	addDoc(collectionRef, data);
 	showNotification('Uploaded!', 4000, 'Success');
@@ -50,18 +49,4 @@ export const pushImage = async (image: File) => {
 			);
 		} else showNotification("Couldn't process file!", 4000, 'Failure');
 	};
-
-	/* 
-	const vision = new ImageAnnotatorClient();
-	const [textDetections] = await vision.textDetection(image);
-	const [annotation] = textDetections.textAnnotations;
-	const text = annotation ? annotation.description.trim() : '';
-	console.log('Extracted text from image:', textDetections);
-
-	fetch(import.meta.env.VITE_HTTPFUNCTION_URL, { method: 'POST', body: image }).then((res) =>
-		res.json().then((result) => {
-			console.log(result);
-		})
-	);
- */
 };
